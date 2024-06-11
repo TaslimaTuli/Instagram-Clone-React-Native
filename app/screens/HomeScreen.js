78c022b7a496399f77d36997345932f7caeffc3e
Taslima Akter Tuli <ajraftuli@gmail.com>
2024-06-11 17:26:53 +0600

    .

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,11 +1,22 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
-import Body from "../components/Home/Body";
+
+import Stories from "../components/Home/Stories";
+import Posts from "../components/Home/Posts";
+import Header from "../components/Home/Header";
 
 export default function Home() {
 	return (
-		<SafeAreaView>
-			<Body />
-		</SafeAreaView>
+		<View className="">
+			<SafeAreaView>
+				<ScrollView showsHorizontalScrollIndicator={false}>
+					<Header />
+					<Stories />
+					<Posts />
+					<Posts />
+					<Posts />
+				</ScrollView>
+			</SafeAreaView>
+		</View>
 	);
 }
